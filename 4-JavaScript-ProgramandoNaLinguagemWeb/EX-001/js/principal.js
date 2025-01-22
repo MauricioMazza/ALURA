b8912c77740c6785ec242a88b19975470a42a8ee
Mauricio Mazza <mauriciomazzadev@gmail.com>
2025-01-22 10:14:06 -0300

    curso de JavaScript

--- 4-JavaScript-ProgramandoNaLinguagemWeb/EX-001/js/principal.js
+++ 4-JavaScript-ProgramandoNaLinguagemWeb/EX-001/js/principal.js
@@ -11,8 +11,32 @@ for(var i = 0 ; i < pacientes.length ; i++) {
     var altura = tdAltura.textContent
 
     var tdImc = paciente.querySelector('.info-imc')
-    
-    var imc = peso / (altura * altura)
 
-    tdImc.textContent = imc.toFixed(1)
+    var pesoEhValido = true
+    var alturaEhValida = true
+
+    if(peso <= 0 || peso >= 700) {
+        console.log('Peso inválido!')
+        pesoEhValido = false        
+        tdImc.textContent = "Peso inválido!"
+        paciente.classList.add('paciente-invalido')        
+    }
+
+    if(altura <= 0 || altura >= 3) {
+        console.log('Altura inválida!')
+        alturaEhValida = false        
+        tdImc.textContent = 'Altura inválida!'
+        paciente.classList.add('paciente-invalido')
+    }
+
+    if(alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura)
+        tdImc.textContent = imc.toFixed(1)
+    } 
+
+    if(alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura)
+        tdImc.textContent = imc.toFixed(1)
+    }       
+    
 }
